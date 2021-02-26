b34fbe429a2a2fa0469a18af047ac585be580c7f
Jollokim <joakimjensenj@gmail.com>
2021-02-26 15:00:43 +0100

    init Scraper class

--- code/src/main/java/Scraper.java
+++ code/src/main/java/Scraper.java
@@ -1,2 +1,27 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Scraper {
+    private String URL;
+    private String websiteContent;
+
+    public Scraper(String URL) throws IOException {
+        this.URL = URL;
+
+        //Instantiating the URL class
+        URL url = new URL(URL);
+        //Retrieving the contents of the specified page
+        Scanner sc = new Scanner(url.openStream());
+        //Instantiating the StringBuffer class to hold the result
+        StringBuffer sb = new StringBuffer();
+
+        while(sc.hasNext()) {
+            sb.append(sc.next());
+            //System.out.println(sc.next());
+        }
+        
+        //Retrieving the String from the String Buffer object
+        this.websiteContent = sb.toString();
+    }
 }
