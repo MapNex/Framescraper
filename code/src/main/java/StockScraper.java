a08865c61203caff21c5eae0aaa220697ea3cd3d
MapNex <54624778+MapNex@users.noreply.github.com>
2021-03-01 11:34:21 +0100

    La til StockScraper

---
+++ code/src/main/java/StockScraper.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+public class StockScraper extends ConceptScraper {
+
+    public StockScraper(String url) throws IOException{
+
+        super(url);
+    }
+
+    public String[] getStockInfo(String aksjeTicker){
+        return null;
+    }
+
+    public Double getStockReturn(String aksjeTicker, int kjoptKurs, int antallAksjer){
+        return null;
+    }
+
+    public Double getStockReturnPrcent(String aksjeTicker, int kjoptkurs, int antallAksjer){
+        return null;
+    }
+
+
+}
