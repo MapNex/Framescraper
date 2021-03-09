098de39e19731d409b8a3980cb21f83b1a4ae805
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-03-09 21:19:23 +0100

    First test with java doc

--- code/src/main/java/ConceptScraper.java
+++ code/src/main/java/ConceptScraper.java
@@ -1,12 +1,26 @@
 import java.io.IOException;
 
 public abstract class ConceptScraper {
+
+    /**
+     * This is a abstract class to let the predefined
+     * classes use the methods from Scraper with correct inheriting.
+     */
+
     private Scraper scraper;
 
+    /**
+     * @param URL Takes a string with the URL as parameter. This is forwarded to the Scraper
+     *           and instantiates a new object from the webpage.
+     * @throws IOException If the string sent in could not be processed as intended.
+     */
     public ConceptScraper(String URL) throws IOException {
         scraper = new Scraper(URL);
     }
-    
+
+    /**
+     * @return A object of the Scraper class which all the Scrapers methods can be applied to.
+     */
     public Scraper getScraper(){
         return scraper;
     }
