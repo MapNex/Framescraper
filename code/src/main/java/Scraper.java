da298cd1ba066216d988da3f0d65c954e01dda96
Jollokim <joakimjensenj@gmail.com>
2021-03-10 17:03:26 +0100

    javadocs comments til Scraper

--- code/src/main/java/Scraper.java
+++ code/src/main/java/Scraper.java
@@ -2,10 +2,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Scanner;
 
+/**
+ * Shopscraper class representing a specific instantiation of a ConceptScraper
+ * @author Mathias Jarbekk
+ * @author Thomas Johannessen
+ * @author Joakim Jensen
+ * @author Michal Kowalski
+ * @version 1.0
+ * @see ConceptScraper
+ */
 public class Scraper {
     private String URL;
     private String websiteContent;
 
+
+    /**
+     * Constructor for a shop scraper
+     * @param URL Link to target site for scraping
+     * @throws IOException Throws an IO Exception whenever user input is crashing with an expected string value
+     */
     public Scraper(String URL) throws IOException {
         this.URL = URL;
 
@@ -25,38 +40,78 @@ public class Scraper {
         this.websiteContent = sb.toString();
     }
 
+    /**
+     *
+     * @return The url as a String
+     */
     public String getURL() {
         return URL;
     }
 
+    /**
+     *
+     * @return The website content as String
+     */
     public String getWebsiteContent() {
         return websiteContent;
     }
 
+    /**
+     *
+     * @param tag The tag from which we want to see the content of
+     * @return The content is returned as Strings in String[]
+     */
     public String[] getContentFromTag(String tag){
         return null;
     }
 
+    /**
+     *
+     * @param id The id of an element which we want to get the content from
+     * @return returns the content as String in String[]
+     */
     public String[] getContentFromId(String id){
         return null;
     }
 
+    /**
+     *
+     * @param className The class name of the element which we want to get the content of
+     * @return
+     */
     public String[] getContentFromClass(String className){
         return null;
     }
 
+    /**
+     *
+     * @return the links as String in String[]
+     */
     public String[] getLinksInPage(){
         return null;
     }
 
+    /**
+     *
+     * @param searchString string we want to see if exist in the HTML code
+     * @return returns boolean based on if the String was found or not. True for was found and false for was not found.
+     */
     public boolean contains(String searchString){
         return true;
     }
 
+    /**
+     *
+     * @return all found classes as strings in String[]
+     */
     public String[] getClassesInPage(){
         return null;
     }
 
+    /**
+     *
+     * @return all found id's as strings in String[]
+     */
     public String[] getIdsInPage(){
         return null;
     }
