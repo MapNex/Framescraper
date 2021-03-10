6b431af96cd0faadf81694b178ecc5e47d65dd95
ThomasJohannessen <54627448+ThomasJohannessen@users.noreply.github.com>
2021-03-10 18:36:06 +0100

    Added java docs to the code for ConceptScraper

--- code/src/main/java/ConceptScraper.java
+++ code/src/main/java/ConceptScraper.java
@@ -1,24 +1,32 @@
 import java.io.IOException;
 
-public abstract class ConceptScraper {
+/**
+ * ConceptScraper is a abstract class to let the predefined
+ * classes use the methods from Scraper with correct inheriting.
+ * @author Mathias Jarbekk
+ * @author Thomas Johannessen
+ * @author Joakim Jensen
+ * @author Michal Kowalski
+ * @version 1.0
+ */
 
-    /**
-     * This is a abstract class to let the predefined
-     * classes use the methods from Scraper with correct inheriting.
-     */
+public abstract class ConceptScraper {
 
     private Scraper scraper;
 
     /**
-     * @param URL Takes a string with the URL as parameter. This is forwarded to the Scraper
+     * Constructor for ConceptScraper, which is used by the predefined classes to access the Scraper
+     * @param URL String with the URL. This is forwarded to the Scraper
      *           and instantiates a new object from the webpage.
-     * @throws IOException If the string sent in could not be processed as intended.
+     * @throws IOException Throws if the string sent in could not be processed as intended.
      */
     public ConceptScraper(String URL) throws IOException {
         scraper = new Scraper(URL);
     }
 
     /**
+     * Used to access the Scraper, and apply the Scrapers methods to the objects from ShopScraper, StockScraper
+     * and for user instantiated objects.
      * @return A object of the Scraper class which all the Scrapers methods can be applied to.
      */
     public Scraper getScraper(){
